a43012d57cac70a1a49a1c8b95a7f6e1533de2d3
Domenico <bellavia-d@hotmail.com>
2018-11-28 13:01:55 +0100

    js portfolio terminé

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,26 +1,31 @@
-let portfolio = [];
+let tab1 = [];
+tab1 = document.querySelectorAll('.web');
+let tab2 = [];
+tab2 = document.querySelectorAll('.design');
 
-for (let i = 0; i < portfolio.length; i++) {
-    document.querySelector('.btnall').addEventListener('click', function () {
-
-        portfolio[i].style.display = 'block';
-
-    })
-    document.querySelector('.btndesign').addEventListener('click', function () {
-
-        if (portfolio[i].classList.contains('web')) {
-            portfolio[i].style.display = "none"
-        } else {
-            portfolio[i].style.display = "block"
-        }
-
-    })
-    document.querySelector('.btnweb').addEventListener('click', function () {
-
-        if (portfolio[i].classList.contains('web')) {
-            portfolio[i].style.display = "block"
-        } else {
-            portfolio[i].style.display = "none"
-        }
-    })
+function design() {
+    for (let i = 0; i < tab1.length; i++) {
+        tab1[i].style.display = "none";
+        tab2[i].style.display = "block";
+    }
 }
+document.querySelector(".btndesign").addEventListener('click', design);
+
+function web() {
+    for (let i = 0; i < tab2.length; i++) {
+        tab2[i].style.display = "none";
+        tab1[i].style.display = "block";
+    }
+}
+document.querySelector(".btnweb").addEventListener('click', web);
+
+function all() {
+    console.log('salut')
+    for (let i = 0; i < tab1.length && i < tab2.length; i++) {
+        tab1[i].style.display = "block";
+        tab2[i].style.display = "block";
+    }
+}
+document.querySelector(".btnall").addEventListener('click', all);
+
+
